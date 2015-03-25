678c84398477787120458cc2ecae9002dde01756
Myles Braithwaite <me@mylesbraithwaite.com>
2015-03-25 14:17:45 -0400

    Did some stuff.

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		aws: grunt.file.readJSON('.aws.json'),
+		
+		s3: {
+			options: {
+				accessKeyId: "<%= aws.accessKeyId %>",
+				secretAccessKey: "<%= aws.secretAccessKey %>",
+				bucket: "myles.coffee"
+			},
+			build: {
+				cwd: "build",
+				src: "**"
+			}
+		},
+		exec: {
+			encrypt: {
+				cmd: "gpg --encrypt --armor -r <%= config.gpg_key => --batch --yes --trust-model always -o .aws.json.gpg .aws.json"
+			},
+			decrypt: {
+				cmd: "gpg --decrypt --armor -r <%= config.gpg_key => --batch --yes --trust-model always -o .aws.json .aws.json.gpg"
+			}
+		},
+		middleman: {
+			options: {
+				useBundle: true
+			},
+			server: {
+				options: {
+					command: "server",
+					environment: 'development'
+				}
+			},
+			build: {
+				options: {
+					command: "build",
+					environment: 'production'
+				}
+			}
+		}
+	});
+	
+	grunt.loadNpmTasks('grunt-aws');
+	grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks('grunt-middleman');
+	
+	grunt.registerTask('default', ['middleman:build']);
+	grunt.registerTask('run', ['middleman:server']);
+	grunt.registerTask('deploy', ['default', 's3']);
+};
