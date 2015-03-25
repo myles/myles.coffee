01327718b5423a1b92e562985036932ce2a4c8a6
Myles Braithwaite <me@mylesbraithwaite.com>
2015-03-25 14:02:37 -0400

    Initial commit.

---
+++ config.rb
@@ -0,0 +1,33 @@
+require "extensions/views"
+
+activate :views
+activate :directory_indexes
+
+set :relative_link, true
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+set :fonts_dir, 'assets/fonts'
+set :layout, 'layouts/application'
+
+configure :development do
+  activate :livereload
+end
+
+configure :build do
+  # Relative assets needed to deploy to GitHub Pages
+  activate :relative_assets
+end
+
+helpers do
+  def nav_link(link_text, page_url, options = {})
+    options[:class] ||= ""
+    if current_page.url.length > 1
+      current_url = current_page.url.chop
+    else
+      current_url = current_page.url
+    end
+    options[:class] << " active" if page_url == current_url
+    link_to(link_text, page_url, options)
+  end
+end
